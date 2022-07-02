46fb8e1c595693c3b526607abfac3f0f4625ed6a
MichaelStott <stottmichael2@gmail.com>
2022-07-02 16:46:49 -0400

    Adding golib health checks

--- go/app/main.go
+++ go/app/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 )
 
 func main() {
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	conn, err := pgx.Connect(context.Background(), "postgres://admin:admin@skaffold-init-db-postgresql:5432/polardb_catalog")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
 	r := gin.Default()
 	r.GET("/health", func(c *gin.Context) {
+		err = conn.QueryRow(context.Background(), "SELECT version();").Scan()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			panic(err)
+		}
 		c.JSON(http.StatusOK, gin.H{
 			"message": "OK",
 		})
