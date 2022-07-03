614d5ab1ec7612dde9fac3181caa05ee456311d8
MichaelStott <stottmichael2@gmail.com>
2022-07-03 16:17:51 -0400

    Add db healthcheckc

--- go/app/main.go
+++ go/app/main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	// urlExample := "postgres://username:password@localhost:5432/database_name"
-	conn, err := pgx.Connect(context.Background(), "postgres://admin:admin@skaffold-init-db-postgresql:5432/polardb_catalog")
+	conn, err := pgx.Connect(context.Background(), "postgres://skaffold:password@skaffold-init-db-postgresql:5432/skaffold_init")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
@@ -21,7 +21,8 @@ func main() {
 
 	r := gin.Default()
 	r.GET("/health", func(c *gin.Context) {
-		err = conn.QueryRow(context.Background(), "SELECT version();").Scan()
+		var valueStr string
+		err = conn.QueryRow(context.Background(), "SELECT version();").Scan(&valueStr)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 			panic(err)
